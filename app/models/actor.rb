8427c0c4cada71ed614fe4ffcb6e07c2f4a8187c
alantotah <alantotah@users.noreply.github.com>
2017-05-18 17:11:34 -0400

    msm_v5

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,6 +1,10 @@
 class Actor < ApplicationRecord
   # - name: must be present; must be unique in combination with dob
   validates :name, :presence => true, :uniqueness => { :scope => :dob}
+
+
+  has_many :actors, :class_name => "Character", :foreign_key => "actor_id"
+
   # - dob: no rules
   # - bio: no rules
   # - image_url: no rules
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -8,7 +8,12 @@ class Movie < ApplicationRecord
     :greater_than_or_equal_to => 1870, :less_than_or_equal_to => 2020 }
     # - duration: must be integer between 0 and 2764800
     validates :duration, :numericality => { :only_integer => true,
-      :greater_than_or_equal_to => 0, :less_than_or_equal_to => 2764800 }
+      :greater_than_or_equal_to => 0, :less_than_or_equal_to => 2764800, :allow_blank => true}
       # - description: no rules
       # - image_url: no rules
+
+      belongs_to :director, :class_name => "Director", :foreign_key => "director_id"
+
+      has_many :characters, :class_name => "Character", :foreign_key => "movie_id"
+
     end
